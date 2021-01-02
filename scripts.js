e7ab108700cfbfc2f69aee26504fa2bbc08d8356
Krupa-Ghetia <krips2407@gmail.com>
2021-01-03 03:07:29 +0530

    Move search options to page top

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
+const mainContainer = document.querySelector("#main-container");
+const container = document.querySelector("#container");
 const searchBar = document.querySelector("#search-bar");
 const searchBtn = document.querySelector("#search-btn");
+const logo = document.querySelector("#logo");
 
 
 const fetchSuggestions = async function (searchValue) {
@@ -8,12 +11,28 @@ const fetchSuggestions = async function (searchValue) {
     return response.json();
 };
 
+const moveSearch = function () {
+    mainContainer.classList.remove("display");
+
+    container.classList.remove("display");
+    container.classList.remove("container");
+    container.classList.add("container-shrink");
+
+    logo.classList.remove("logo");
+    logo.classList.add("logo-shrink");
+
+    searchBtn.classList.remove("search-btn");
+    searchBtn.classList.add("search-btn-shrink");
+}
+
 const search = async function () {
     const searchValue = searchBar.value;
 
     if (!searchValue) {
         return;
     }
+    
+    moveSearch();
 
     const response = await fetchSuggestions(searchValue);
     const suggestions = response["data"].map((suggestion) => {
